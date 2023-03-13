ecb0635ec23a85cd4374b0fcd38b3fc57c43845e
vivignutz <vgnutzmann@gmail.com>
2023-03-13 10:59:18 +0000

    Setted axios interceptors for automatic refresh access token

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -17,7 +17,7 @@ import appStyles from "../../App.module.css";
 import { useSetCurrentUser } from "../../contexts/CurrentUsercontext";
 
 function SignInForm() {
-  const setCurrentUser = SetCurrentUserContext();
+  const setCurrentUser = useSetCurrentUser();
 
   const [signInData, setSignInData] = useState({
     username: "",
