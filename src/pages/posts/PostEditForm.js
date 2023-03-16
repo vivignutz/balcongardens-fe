a76e41b854a684ae4864140f9ab5a65c13c95338
vivignutz <vgnutzmann@gmail.com>
2023-03-16 11:39:09 +0000

    Add commenteditform and updated postpage

--- src/pages/posts/PostEditForm.js
+++ src/pages/posts/PostEditForm.js
@@ -14,18 +14,6 @@ import btnStyles from "../../styles/Button.module.css";
 
 import { useHistory, useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
-import { Figure } from "react-bootstrap";
-
-/*import NavBar from "./components/NavBar";
-import { Route, Switch } from "react-router-dom";
-import SignUpForm from "./pages/auth/SignUpForm";
-import SignInForm from "./pages/auth/SignInForm";
-import PostCreateForm from "./pages/posts/PostCreateForm";
-import PostPage from "./pages/posts/PostPage";
-import PostsPage from "./pages/posts/PostsPage";
-import { useCurrentUser } from "./contexts/CurrentUserContext";
-import PostEditForm from "./pages/posts/PostEditForm";
-*/
 
 /* Component that includes the form for editing/updating
 posts. Includes error handling for input fields*/
@@ -52,7 +40,7 @@ const PostEditForm = () => {
     const handleMount = async () => {
       try {
         const { data } = await axiosReq.get(`/posts/${id}/`);
-        const { title, content, image, is_owner, email, city } = data;
+        const { title, content, image, email, city, is_owner } = data;
 
         is_owner
           ? setPostData({ title, content, image, email, city })
@@ -178,21 +166,21 @@ const PostEditForm = () => {
       <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
         save
       </Button>
-    </>
+    </div>
   );
 
   return (
     <Form onSubmit={handleSubmit}>
       <Row>
         <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
-            <Container
-                className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
-        >
+          <Container
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+          >
             <Form.Group className="text-center">
-                <Figure>
-                    <Image className={appStyles.Image} src={image} rounded />
-                </Figure>
-                <div>
+              <Figure>
+                <Image className={appStyles.Image} src={image} rounded />
+              </Figure>
+              <div>
                 <Form.Label
                   className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
                   htmlFor="image-upload"
@@ -207,7 +195,6 @@ const PostEditForm = () => {
                 onChange={handleChangeImage}
                 ref={imageInput}
               />
-
 
               {errors?.image?.map((message, idx) => (
                 <Alert variant="warning" key={idx}>
@@ -238,7 +225,7 @@ const PostEditForm = () => {
             ))}
 
             <div className="d-md-none">{textFields}</div>
-            </Container>
+          </Container>
         </Col>
         <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
           <Container className={appStyles.Content}>{textFields}</Container>
--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -10,11 +10,18 @@ import { axiosReq } from "../../api/axiosDefaults";
 //import PopularProfiles from "../profiles/PopularProfiles";
 import Post from "./Post";
 
+import CommentCreateForm from "../../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
 // Component used to create the complete postpage
 // Imports Post and Popularprofiles component
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
+
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
@@ -35,12 +42,24 @@ function PostPage() {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <PopularProfiles mobile />
+        <p>Popular profiles for mobile</p>
         <Post {...post.results[0]} setPosts={setPost} postPage />
-        <Container className={appStyles.Content}>Comments</Container>
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+        </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        <PopularProfiles />
+        Popular profiles for desktop
       </Col>
     </Row>
   );
