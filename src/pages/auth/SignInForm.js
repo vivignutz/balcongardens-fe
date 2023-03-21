09dfae6e2daa5da890ec5fb75d7cac9b5849fab9
vivignutz <vgnutzmann@gmail.com>
2023-03-21 08:13:25 +0000

    Change token configuration for tests

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -16,7 +16,7 @@ import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 import { useRedirect } from "../../hooks/useRedirect";
-import { setTokenTimestamp } from "../../utils/utils";
+// import { setTokenTimestamp } from "../../utils/utils";
 
 function SignInForm() {
   const setCurrentUser = useSetCurrentUser();
@@ -37,7 +37,7 @@ function SignInForm() {
     try {
       const { data } = await axios.post("/dj-rest-auth/login/", signInData);
       setCurrentUser(data.user);
-      setTokenTimestamp(data);
+      // setTokenTimestamp(data);
       history.goBack();
     } catch (err) {
       setErrors(err.response?.data);
